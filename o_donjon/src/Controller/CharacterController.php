22323957ad837a9af69ecb801d4f8025e4a71933
CecileBouche <cecile.bouche@free.fr>
2021-05-06 19:00:39 +0200

    feat(character-controller): add function to edit campaign

--- o_donjon/src/Controller/CharacterController.php
+++ o_donjon/src/Controller/CharacterController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Campaign;
 use App\Entity\Character;
 use App\Form\CharacterType;
+use App\Repository\CampaignRepository;
 use App\Repository\CharacterRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
      * @Route("/character", name="character")
+     * 
      */
 class CharacterController extends AbstractController
 {
@@ -106,7 +110,49 @@ class CharacterController extends AbstractController
         return $this->json(null, 204);
     }
 
+    /************************************ADVANCED METHODS*************************************/
 
+    /**
+     * ASSIGN A CAMPAIGN TO A CHARACTER
+     * 
+     * @Route("/{id}/campaign/{campaign_id}", name="edit_campaign", methods={"PATCH"})
+     * @ParamConverter("character", options={"mapping": {"id": "id"}})
+     * @ParamConverter("campaign", options={"mapping": {"campaign_id": "id"}})
+     * 
+     */
+    public function editCampaign(Request $request, Character $character, Campaign $campaign): Response
+    {
+        // Identify campaign Id
+        $campaignId = $campaign->getId();
+
+        // Associate form and character 
+        $form = $this->createForm(CharacterType::class, $character, [
+            'csrf_protection' => false
+        ]);
+        
+        // Transform request json content
+        $sentData = json_decode($request->getContent(), true);
+        
+        // Assign new campaign Id to the request character  
+        $sentData['campaign'] = $campaignId;
+
+        // Submit updated data
+        $form->submit($sentData);
+
+        // Check and responses
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->json(
+                $character, 200, [], [
+                    'groups' => ['read_character'],
+                ]
+            );
+        }
+        return $this->json($form->getErrors(true, false)->__toString(), 400);
+        
+    }
 
 
 
