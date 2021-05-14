df1c1562c4e5047fb9a41ea8754e6882c623ae7a
codesieg <sieg.heart@hotmail.fr>
2021-05-14 10:29:56 +0200

    fix[mercurebundle]: fix mercure bundle

--- o_donjon/config/bundles.php
+++ o_donjon/config/bundles.php
@@ -15,5 +15,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
