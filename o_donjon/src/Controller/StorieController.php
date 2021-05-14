07c074ff3f98124cc5960cbf4a0f99a709393630
nael-broussard <nael.broussard@2019.icam.fr>
2021-05-14 15:58:28 +0200

    info[StoryController]: add comments

--- o_donjon/src/Controller/StorieController.php
+++ o_donjon/src/Controller/StorieController.php
@@ -20,8 +20,11 @@ class StorieController extends AbstractController
      * @Route("", name="browse", methods={"GET"})
      */
     public function browse(StoryRepository $storyRepository): Response
-    {
+    {   
+        // on récupère les histoires
         $stories = $storyRepository->findAll();
+
+        // on retourne la liste des histoires
         return $this->json($stories, 200, [], [
             'groups' => ['browse_story'],
         ]);
@@ -31,24 +34,34 @@ class StorieController extends AbstractController
      * @Route("", name="add", methods={"POST"})
      */
     public function add(Request $request): Response
-    {
+    {   
+        // on créer un objet Story
         $story = new Story();
+
+        // on créer un formulaire pour la classe
         $form = $this->createForm(StoryType::class, $story, ['csrf_protection' => false]);
-
+        
+        // on récupère les informations de la requête
         $sentData = json_decode($request->getContent(), true);
-        $form->submit($sentData);
 
+        // on envoie les informations dans le formulaire
+        $form->submit($sentData);
+        
+        // si les données sont valides
         if ($form->isValid()) {
 
+            // on envoie les données à la BDD
             $em = $this->getDoctrine()->getManager();
             $em->persist($story);
             $em->flush();
 
+            // on retourne l'histoire créée
             return $this->json($story, 201, [], [
                 'groups' => ['read_story'],
             ]);
         }
 
+        // si le formulaire n'est pas valide on retourne les erreurs
         return $this->json($form->getErrors(true, false)->__toString(), 400);
     }
 
@@ -56,7 +69,8 @@ class StorieController extends AbstractController
      * @Route("/{id}", name="read", methods={"GET"}, requirements={"id": "\d+"})
      */
     public function read(Story $story): Response
-    {
+    {   
+        // on retourne l'histoire concernée
         return $this->json($story, 200, [], [
             'groups' => ['read_story'],
         ]);
@@ -66,20 +80,28 @@ class StorieController extends AbstractController
      * @Route("/{id}", name="edit", methods={"PUT", "PATCH"}, requirements={"id": "\d+"})
      */
     public function edit(Story $story, Request $request): Response
-    {
+    {   
+        // on créer un formulaire pour la classe Story
         $form = $this->createForm(StoryType::class, $story, ['csrf_protection' => false]);
 
+        // on récupère les informations de la requête
         $sentData = json_decode($request->getContent(), true);
+
+        // on envoie les informations dans le formulaire
         $form->submit($sentData);
 
+        // si les données sont valides
         if ($form->isValid()) {
 
+            // on envoie les données à la BDD
             $this->getDoctrine()->getManager()->flush();
 
+            // on retourne l'histoire modifiée
             return $this->json($story, 200, [], [
                 'groups' => ['read_story'],
             ]);
         }
+         // si le formulaire n'est pas valide on retourne les erreurs
         return $this->json($form->getErrors(true, false)->__toString(), 400);
     }
 
@@ -87,11 +109,13 @@ class StorieController extends AbstractController
      * @Route("/{id}", name="delete", methods={"DELETE"}, requirements={"id": "\d+"})
      */
     public function delete(Story $story): Response
-    {
+    {   
+        // on supprime l'histoire de la BDD
         $em = $this->getDoctrine()->getManager();
         $em->remove($story);
         $em->flush();
 
+        // on retourne un code 204 si l'histoire a bien été supprimée
         return $this->json(null, 204);
     }
 
