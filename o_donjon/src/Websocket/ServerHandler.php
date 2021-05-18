5689a8fd617ada5bb04cf2fca27f6fbabfc06002
Nicolas Vidal <45036620+Codesieg@users.noreply.github.com>
2021-05-18 16:56:40 +0200

    fix[websocket]: fix websocket (#69)
    * fix[websocket]: fix websocket
    
    * fix[websocket]: port websocket to 8000

--- o_donjon/src/Websocket/ServerHandler.php
+++ o_donjon/src/Websocket/ServerHandler.php
@@ -16,36 +16,35 @@ class ServerHandler implements MessageComponentInterface
         $this->connections = new SplObjectStorage;
     }
     
-    public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
-        $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->connections->attach($conn);
+    
     }
+    // public function onOpen($conn) {
+    //     $conn->send('Hello ' . $conn->Session->get('name'));
+    // }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach($this->connections as $connection)
+        {
+            if($connection === $from)
+            {
+                continue;
             }
+            $connection->send($msg);
         }
     }
 
-    public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
-
-        echo "Connection {$conn->resourceId} has disconnected\n";
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->connections->detach($conn);
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        echo "An error has occurred: {$e->getMessage()}\n";
-
+    public function onError(ConnectionInterface $conn, Exception $e)
+    {
+        $this->connections->detach($conn);
         $conn->close();
     }
 }
