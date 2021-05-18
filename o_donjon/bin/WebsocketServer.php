b242266233e222b35761333496528f3454e480a6
codesieg <sieg.heart@hotmail.fr>
2021-05-18 13:42:21 +0200

    feat[websocket]: first test for tls websocket

--- o_donjon/bin/WebsocketServer.php
+++ o_donjon/bin/WebsocketServer.php
@@ -1,23 +1,27 @@
 <?php
 require dirname( __DIR__ ) . '/vendor/autoload.php';
 
+use React\Socket\Server;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
+use React\Socket\SecureServer;
 use Ratchet\WebSocket\WsServer;
 use App\Websocket\ServerHandler;
 
+$app = new HttpServer(
+    new WsServer(
+        new ServerHandler()
+    )
+);
 
-        $port = 8000;
-        // $output->writeln("Starting server on port " . $port);
-        $server = IoServer::factory(
-            new HttpServer(
-                new WsServer(
-                    new ServerHandler()
-                )
-            ),
-            $port
-        );
-        echo ("Websocket server is running");
-        $server->run();
-        return 0;
-    
+$loop = \React\EventLoop\Factory::create();
+
+$secure_websockets = new Server('8000', $loop);
+$secure_websockets = new SecureServer($secure_websockets, $loop, [
+    'local_cert' => '/etc/letsencrypt/live/odonjon.fr/cert.pem',
+    'local_pk' => '/etc/letsencrypt/live/odonjon.fr/privkey.pem',
+    'verify_peer' => false
+]);
+
+$secure_websockets_server = new IoServer($app, $secure_websockets, $loop);
+$secure_websockets_server->run();
