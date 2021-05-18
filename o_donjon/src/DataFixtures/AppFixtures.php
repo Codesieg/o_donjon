18618cf92dc2a06343935e21a55bbbc9d105d8ea
CecileBouche <cecile.bouche@free.fr>
2021-05-18 16:39:13 +0200

    feat[fixtures]: continue to create fixtures

--- o_donjon/src/DataFixtures/AppFixtures.php
+++ o_donjon/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Character;
+use App\Entity\Race;
+use App\Entity\CharacterClass;
+use App\Entity\Caracteristic;
+use App\Entity\Statistics;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -20,7 +24,10 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $characters = [];
+        
+        /*******************************************************TABLES WITH DATA*******************************************/
+
+        /*****CHARACTER TABLE****************/
 
         $characterNames = [
             'Pikachu',
@@ -158,25 +165,107 @@ class AppFixtures extends Fixture
         ];
 
 
+        /******************RACE TABLE****************/
+
+        $raceNames = [
+            'human',
+            'dwarves',
+            'elves',
+            'halflings',
+            'dragonborn',
+            'gnome',
+            'half-elfe',
+            'half-orc',
+            'tiefling',
+            'aarakocra',
+        ];
+
+
+        /******************CLASS TABLE****************/
+
+        $classNames = [
+            'barbarian',
+            'bard',
+            'cleric',
+            'druid',
+            'fighter',
+            'monk',
+            'paladin',
+            'ranger',
+            'rogue',
+            'sorcerer',
+            'warlock',
+            'wizard',
+        ];
+
+
+        /******************SPELL TABLE*************************/
+
+        $spellCastingClasses = [
+            'human',
+            'dwarves',
+            'elves',
+            'halflings',
+        ];
+
+        /******************SAVING THROWS TABLE*********************/
+
+        $savingsThrowsStrengthes = [
+            'human',
+            'dwarves',
+            'elves',
+            'halflings',
+        ];
+
+        /***********************SKILL TABLE************************/
+
+        $skillAcrobatics = [
+            'human',
+            'dwarves',
+            'elves',
+            'halflings',
+        ];
+
+        /****************************************************CHARACTER CREATION***************************************************/
+
+               
+        $characters = [];
+        $races = [];
+        $classes = [];
+        $caracteristics = [];
+        $statistics = [];
+
         foreach ($characterNames as $characterName) {
+            
             $character = new Character();
+            
             $character->setName($characterName);
+            
             $character->setAvatarPath($characterName . '.png');
+            
             $character->setAge(random_int(0, 300));
+            
             $character->setHeight(random_int(50, 300));
+            
             $character->setWeight(random_int(10, 200));
+            
             shuffle($characterEyes);
-            $character->setEyes(shuffle($characterEyes[1]));
+            $character->setEyes($characterEyes[0]);
+            
             shuffle($characterSkins);
-            $character->setSkin(shuffle($characterSkins[1]));
+            $character->setSkin($characterSkins[1]);
+            
             shuffle($characterHairs);
-            $character->setHair(shuffle($characterHairs[1]));
+            $character->setHair($characterHairs[1]);
+            
             shuffle($appearance);
-            $character->setAppearance(shuffle($appearance[1]));
+            $character->setAppearance($appearance[1]);
+            
             shuffle($personnalityTraits);
-            $character->setPersonalityTraits(shuffle($personnalityTraits[1]));
+            $character->setPersonalityTraits($personnalityTraits[1]);
+            
             shuffle($ideals);
-            $character->setIdeals(shuffle($ideals[1]));
+            $character->setIdeals($ideals[1]);
 
             shuffle($words);
             $bonds = '';
@@ -186,30 +275,109 @@ class AppFixtures extends Fixture
             $character->setBonds($bonds);
             
             shuffle($flaws);
-            $character->setFlaws(shuffle($flaws[1]));
+            $character->setFlaws($flaws[1]);
+
             shuffle($alliesAndOrganizations);
-            $character->setAlliesAndOrganizations(shuffle($alliesAndOrganizations[1]));
+            $character->setAlliesAndOrganizations($alliesAndOrganizations[1]);
 
             shuffle($words);
             $backstory = '';
-            for ($i=0; $i < rand(30, 50); $i++) { 
-                $backstory = $backstory . " " . $backstory[$i];
+            for ($i = 0; $i < rand(30, 50); $i++) { 
+                $backstory = $backstory . " " . $words[$i];
             };
             $character->setBackstory($backstory);
 
             shuffle($treasure);
-            $character->setTreasure(shuffle($treasure[1]));
+            $character->setTreasure($treasure[1]);
 
             shuffle($background);
-            $character->setBackground(shuffle($background[1]));
+            $character->setBackground($background[1]);
 
+            $race = new Race();
+            $character->setRace($race);
+            $races[] = $race;
+
+            $class = new CharacterClass();
+            $character->setClass($class);
+            $classes[] = $class;
+
+            $caracteristic = new Caracteristic();
+            $character->setCaracteristics($caracteristic);
+            $caracteristics[] = $caracteristic;
+
+            $statistic = new Statistics();
+            $character->setStatistics($statistic);
+            $statistics[] = $statistic;
+            
             $manager->persist($character);
+            
             $characters[] = $character;
         }
 
-        
+        /****************************************************RACE CREATION***************************************************/
 
+        foreach ($races as $race) {
+            
+            shuffle($raceNames);
+            $race->setName($raceNames[1]);
+            
+            shuffle($words);
+            $informations = '';
+            for ($i = 0; $i < rand(1, 5); $i++) { 
+                $informations = $informations . " " . $words[$i];
+            };
+            $race->setInformations($informations);
+            
+        }
+
+        /****************************************************CLASS CREATION***************************************************/
+
+        foreach ($classes as $class) {
+            
+            shuffle($classNames);
+            $class->setName($classNames[1]);
+            
+            shuffle($words);
+            $informations = '';
+            for ($i = 0; $i < rand(1, 5); $i++) { 
+                $informations = $informations . " " . $words[$i];
+            };
+            $class->setInformations($informations);
+            
+        }
+
+        /****************************************************CARACTERISTIC CREATION***************************************************/
+
+        foreach ($caracteristics as $caracteristic) {
+            
+            $caracteristic->setLevel(random_int(1, 20));
+            $caracteristic->setExperience(random_int(1, 100));
+            $caracteristic->setInspiration((bool)random_int(0, 1));
+            $caracteristic->setArmorClass(random_int(1, 10));
+            $caracteristic->setSpeed(random_int(1, 30));
+            $caracteristic->setCurrentHP(random_int(1, 100));
+            $caracteristic->setTotalHP(random_int(1, 100));
+            $caracteristic->setHitDice(random_int(1, 20));
+            $caracteristic->setDeathSavesSuccess(random_int(1, 10));
+            $caracteristic->setDeathSavesFailures(random_int(1, 10));
+      
+            
+        }
+
+        /****************************************************STATISTIC CREATION***************************************************/
+
+        foreach ($statistics as $statistic) {
+            
+            $statistic->setStrength(random_int(1, 20));
+            
+            
+        }
+
+        /******************************************************************************************************************************/
+        /****************************************************SENDING TO THE DB*********************************************************/
+        /******************************************************************************************************************************/
 
         $manager->flush();
+
     }
 }
