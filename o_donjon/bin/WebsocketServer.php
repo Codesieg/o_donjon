270ba2f8dc283645a376a03a869642d155a2a05e
codesieg <sieg.heart@hotmail.fr>
2021-05-18 14:42:57 +0200

    feat[websocket]: active proxy serve

--- o_donjon/bin/WebsocketServer.php
+++ o_donjon/bin/WebsocketServer.php
@@ -9,20 +9,33 @@ use React\Socket\SecureServer;
 use Ratchet\WebSocket\WsServer;
 use App\Websocket\ServerHandler;
 
-$app = new HttpServer(
-    new WsServer(
-        new ServerHandler()
-    )
-);
+// $app = new HttpServer(
+//     new WsServer(
+//         new ServerHandler()
+//     )
+// );
 
-$loop = Factory::create();
+// $loop = Factory::create();
 
-$secure_websockets = new Server(8000, $loop);
-$secure_websockets = new SecureServer($secure_websockets, $loop, [
-    'local_cert' => '/etc/letsencrypt/live/odonjon.fr/cert.pem',
-    'allow_self_signed' => true,
-    'verify_peer' => false
-]);
+// $secure_websockets = new Server(8000, $loop);
+// $secure_websockets = new SecureServer($secure_websockets, $loop, [
+//     'local_cert' => '/etc/letsencrypt/live/odonjon.fr/cert.pem',
+//     'allow_self_signed' => true,
+//     'verify_peer' => false
+// ]);
 
-$secure_websockets_server = new IoServer($app, $secure_websockets, $loop);
-$secure_websockets_server->run();
+// $secure_websockets_server = new IoServer($app, $secure_websockets, $loop);
+// $secure_websockets_server->run();
+
+        $port = 8000;
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new ServerHandler()
+                )
+            ),
+            $port
+        );
+        echo ("Websocket server is running");
+        $server->run();
+        return 0;
