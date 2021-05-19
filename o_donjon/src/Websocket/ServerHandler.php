56e5c3fedacdf51809eb90fdd30a0e41a7a5d60b
Nicolas Vidal <45036620+Codesieg@users.noreply.github.com>
2021-05-19 11:43:11 +0200

    Websocket/ssl (#73)
    * fix[websocket]: fix websocket
    
    * fix[websocket]: port websocket to 8000
    
    * feat[websocket]: websocket local conf
    
    * feat[websocket]: methode onMessage
    
    * feat[controller]: methode onMessage
    
    * feat[controller]: methode onMessage 2

--- o_donjon/src/Websocket/ServerHandler.php
+++ o_donjon/src/Websocket/ServerHandler.php
@@ -21,19 +21,17 @@ class ServerHandler implements MessageComponentInterface
         $this->connections->attach($conn);
     
     }
-    // public function onOpen($conn) {
-    //     $conn->send('Hello ' . $conn->Session->get('name'));
-    // }
 
-    public function onMessage(ConnectionInterface $from, $msg)
-    {
-        foreach($this->connections as $connection)
-        {
-            if($connection === $from)
-            {
-                continue;
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
             }
-            $connection->send($msg);
         }
     }
 
